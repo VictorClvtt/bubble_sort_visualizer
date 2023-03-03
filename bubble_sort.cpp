b0cac0cdfe12f90a3aafaf0a33191a0d18a8fe74
Victor Clivatti <joaoclivatti@hotmail.com>
2023-03-03 00:09:50 -0300

    Visual Corrections

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
 #include <algorithm>
+#include <windows.h>
+
+void eraseLines(int n) {
+    COORD pos = {0, 0};
+    CONSOLE_SCREEN_BUFFER_INFO info;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // Get the console cursor position and buffer information
+    GetConsoleScreenBufferInfo(hConsole, &info);
+
+    // Calculate the position to start erasing from
+    pos.Y = info.dwCursorPosition.Y - n + 1;
+    if (pos.Y < 0) {
+        pos.Y = 0;
+    }
+
+    // Set the cursor position to the starting position
+    SetConsoleCursorPosition(hConsole, pos);
+
+    // Erase the desired number of lines
+    for (int i = 0; i < n; i++) {
+        std::cout << std::string(info.dwMaximumWindowSize.X, ' ');
+        SetConsoleCursorPosition(hConsole, pos);
+        pos.Y++;
+    }
+
+    // Set the cursor position to the beginning of the first erased line
+    pos.Y -= n;
+    SetConsoleCursorPosition(hConsole, pos);
+}
 
 void displayBars(int vet[]){
 	for(int j=0;j<=9;j++){
@@ -8,10 +38,6 @@ void displayBars(int vet[]){
 		}
 		std::cout << "[" << vet[j] << "]" << std::endl; 
 	}
-	
-	for (int i=0;i<=9;i++){
-        std::cout << "\033[2J\033[1;1H" << std::endl;
-    }
 }
 
 
@@ -24,24 +50,29 @@ int main(){
 		std::cin >> vet[i];
 	}
 	
-	displayBars(vet);
 	std::cout << std::endl;
 	
+	std::cout << "Unsorted array:" << std::endl;
+	displayBars(vet);
+	
+	std::cout << std::endl;
+	
+	std::cout << "Sorted array:" << std::endl;
 	for(j=0;j<=8;j++){
 		for(i=0;i<=8;i++){
 			if(vet[i]>vet[i+1]){
 				std::swap(vet[i],vet[i+1]);
-				
-				displayBars(vet);
-				
-				std::cout << std::endl;
-			}
+				}
 		}
-	}
-	
-	for(i=0;i<=9;i++){
-		std::cout << "-NUMERO " << i+1 << ": " << vet[i] << std::endl;
+		
+		displayBars(vet);
+				
+		if(j<8){
+			eraseLines(11);
+		}
+		
 	}
 	
 	return 0;
+	
 }
